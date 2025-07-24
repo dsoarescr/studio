f0d1fd1a1f154a90ef2dc20cde72bd4e9f4ff669
Diogo Soares <soparajogosxd999@gmail.com>
2025-07-24 00:53:32 +0000

    File changes

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -12,10 +12,10 @@ import type { FirebaseOptions } from 'firebase/app';
  * https://console.firebase.google.com/project/pixel-universe-ub7uk/settings/general/
  */
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyCnAd3LUAS0D1AqulN314WxYNByMG2THDs",
-  authDomain: "pixel-universe-ub7uk.firebaseapp.com",
   projectId: "pixel-universe-ub7uk",
-  storageBucket: "pixel-universe-ub7uk.appspot.com",
-  messagingSenderId: "1071987483660",
-  appId: "1:1071987483660:web:1a8b9c7d6e5f4g3h2i1j0k"
+  appId: "1:973812314333:web:f6e74922db1673d2d62b34",
+  storageBucket: "pixel-universe-ub7uk.firebasestorage.app",
+  apiKey: "AIzaSyCN0aLeKTbE1gYlTX5kwebz63kbhIUR9ug",
+  authDomain: "pixel-universe-ub7uk.firebaseapp.com",
+  messagingSenderId: "973812314333"
 };
