243ed5c60e9e298f0c301dbf260b380685057662
Diogo Soares <109541891+dsoarescr@users.noreply.github.com>
2025-08-14 23:31:17 +0100

    Delete app directory

--- app/(main)/premium/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import PremiumSubscription from '@/components/features/PremiumSubscription';
-
-export default function PremiumPage() {
-  return <PremiumSubscription />;
-}
