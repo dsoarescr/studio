61b5688b7f51052336c0b8cf1bbec60dd3ad276a
Diogo Soares <soparajogosxd999@gmail.com>
2025-08-18 01:46:06 +0000

    continua igual, verifica a imagem anexada mais perto e vê que o cabeçaho

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,9 +11,7 @@ export default function MainLayout({
     <div className="flex flex-col min-h-screen bg-gradient-to-br from-background via-background/98 to-primary/5 transition-colors duration-300">
       <UserProfileHeader />
       <main className="pt-14 pb-[var(--bottom-nav-height)]">
-        <div className="min-h-full">
-          {children}
-        </div>
+        {children}
       </main>
       <BottomNavBar />
     </div>
