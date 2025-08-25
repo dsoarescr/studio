301cbcdf55ae7d11838df6b864a8a67a549fc497
Diogo Soares <soparajogosxd999@gmail.com>
2025-08-25 02:53:35 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <MapSidebar>
    
    with

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,21 +1,13 @@
-
 // src/app/(main)/page.tsx
 'use client';
 
 import React from 'react';
 import PixelGrid from '@/components/pixel-grid/PixelGrid';
-import MapSidebar from '@/components/layout/MapSidebar';
-import { SidebarProvider } from '@/components/ui/sidebar';
 
 export default function HomePage() {
   return (
-    <SidebarProvider>
-      <div className="relative w-full h-full flex">
-        <MapSidebar />
-        <div className="flex-1 h-full">
-          <PixelGrid />
-        </div>
-      </div>
-    </SidebarProvider>
+    <div className="relative w-full h-full">
+      <PixelGrid />
+    </div>
   );
 }
